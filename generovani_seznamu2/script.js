d4f1e535914210a67cee42fbde3a9c70109c3265
ZdenekZaruba <56403536+ZdenekZaruba@users.noreply.github.com>
2022-01-20 15:03:12 +0100

    test

--- generovani_seznamu2/script.js
+++ generovani_seznamu2/script.js
@@ -1,8 +1,3 @@
 function myFunction() {
-    //alert("test");
-    document.getElementByID.onsubmit("input");
-    event.preventDefault();
-    var line = document.createElement("P");
-    line.innerHTML = document.getElementById("input");
-    document.getElementById("text").appendChild(line);
+  var submit = document.getElementById();
 }
